86cdba8ccb24f45588e8376cbc55aef581abcd56
rajanSgnr <rajansachdeva60@gmail.com>
2014-09-03 11:05:57 +0530

    update
    upload all file to git

---
+++ Reward Brokers.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+ $(function(){
+    $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            });
+    });
+ 
+ 
+ 
